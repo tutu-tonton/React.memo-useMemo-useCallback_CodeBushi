7a21ea1a2a6b84e5aa28fc4ebdd30cfa36e98894
tsutomu-noguchi <50165379+tsutomu-noguchi@users.noreply.github.com>
2020-07-07 20:18:50 +0900

    optimization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import './App.css';
+import ChildComponent from './components/ChildComponent';
+
+// 外に出せば、useCallback要らなくなる
+// const fetchData = (type) => {
+// 	return fetch(`https://jsonplaceholder.typicode.com/${type}`)
+// 		.then((response) => response.json())
+// 		.then((json) => console.log(json));
+// };
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+
+	const array = useMemo(() => {
+		return ['One', 'Two', 'Three'];
+	}, []);
+
+	const fetchData = useCallback((type) => {
+		return fetch(`https://jsonplaceholder.typicode.com/${type}`)
+			.then((response) => response.json())
+			.then((json) => console.log(json));
+	}, []);
+
+	useEffect(() => {
+		fetchData('todos');
+	}, [fetchData]);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Count: {count}</h1>
+				<p>
+					<button
+						type="button"
+						onClick={() => {
+							setCount(count + 1);
+						}}
+					>
+						ADD
+					</button>
+				</p>
+				<ChildComponent title="This is the title" array={array} fetchData={fetchData} />
+			</header>
+		</div>
+	);
 }
 
 export default App;
